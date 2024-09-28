5f23b8425a18db984440db7219a052d2991f98f3
HAJIHYUK <gkwlgurdydydy@naver.com>
2024-09-28 18:11:41 +0900

    초기 커밋.

---
+++ Ram.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include "Ram.h"
+Ram::Ram() {
+	size = 100 * 1024;
+}
+Ram::~Ram() {
+	cout << "메모리 제거됨" << endl;
+}
+char Ram::read(int address) {
+	if (address < 0 || address >= size) {
+		cout << "유효하지 않은 주소입니다."<<endl;
+		return '0';
+	}
+	return mem[address];
+	
+}
+void Ram::write(int address, char value) {
+	if (address >= 0 && address < size) {	
+		mem[address] = value;
+	}
+	else {
+		cout << "유효하지 않은 값 입니다." << endl;
+	}
+
+}
