be657d00253e8fb7c5d1c2ef3a4ee2f48a09a1fc
HAJIHYUK <gkwlgurdydydy@naver.com>
2024-10-05 19:02:17 +0900

    #과제#2 다시수정 배열문을 못봄

--- HW/hw#2/main.cpp
+++ HW/hw#2/main.cpp
@@ -25,74 +25,49 @@ public:
 
 class GamblingGame {
 public:
-    Player player1;
-    Player player2;
+    Player players[2]; // 선수 배열
     string input;
 
     GamblingGame() { // 생성자
     }
 
     void play() {
-        cout << "첫 번째 선수 이름>> ";
-        cin >> player1.name;
-        cout << "두 번째 선수 이름>> ";
-        cin >> player2.name;
+        for (int i = 0; i < 2; i++) {
+            cout << "선수 " << (i + 1) << " 이름>> ";
+            cin >> players[i].name;
+        }
 
-        // cin.ignore()를 사용하여 버퍼에 남아 있는 개행 문자 제거
-        cin.ignore();
+        cin.ignore(); // 버퍼에 남아 있는 개행 문자 제거
 
         while (true) {
-            // 첫 번째 선수의 점수 생성 및 입력 처리
-            player1.makeScore();
-            while (true) {
-                cout << player1.name << ": <Enter>" << endl;
-                getline(cin, input);
+            for (int i = 0; i < 2; i++) {
+                players[i].makeScore();
+                while (true) {
+                    cout << players[i].name << ": <Enter>" << endl;
+                    getline(cin, input);
 
-                if (input.empty()) { //<Enter>값 체크 , 빈값 아닐 시 다시 반복
-                    cout << player1.name << "의 점수: " << player1.score[0]<<" " << player1.score[1] <<" " << player1.score[2] << endl;
-                        
+                    if (input.empty()) { //<Enter>값 체크
+                        cout << players[i].name << "의 점수: " << players[i].score[0] << " " << players[i].score[1] << " " << players[i].score[2] << endl;
 
-                    if (player1.score[0] == player1.score[1] && player1.score[1] == player1.score[2]) {
-                        cout << player1.name << "님이 승리!!" << endl;
-                        return; // 3개 동일 숫자일경우 종료
+                        if (players[i].score[0] == players[i].score[1] && players[i].score[1] == players[i].score[2]) {
+                            cout << players[i].name << "님이 승리!!" << endl;
+                            return; // 3개 동일 숫자일경우 종료
+                        }
+                        else {
+                            cout << "아쉽군요!" << endl;
+                            break; // 3개 동일숫자 아닐경우 다음 선수로
+                        }
                     }
                     else {
-                        cout << "아쉽군요!" << endl;
-                        break; // 3개 동일숫자 아닐경우 다음 선수로
+                        cout << "잘못된 입력입니다. 다시 입력해주세요." << endl;
+                        continue; // 빈값 아닐시 다시반복
                     }
-                }
-                else {
-                    cout << "잘못된 입력입니다. 다시 입력해주세요." << endl;
-                    continue; // 빈값 아닐시 다시반복
-                }
-            }
-
-            // 두 번째 선수의 점수 생성 및 입력 처리
-            player2.makeScore();
-            while (true) {
-                cout << player2.name << ": <Enter>" << endl;
-                getline(cin, input);
-
-                if (input.empty()) { //<Enter>값 체크 , 빈값 아닐 시 다시 반복
-                    cout << player2.name << "의 점수: " << player2.score[0] << " " << player2.score[1] << " " << player2.score[2] << endl;
-
-                    if (player2.score[0] == player2.score[1] && player2.score[1] == player2.score[2]) {
-                        cout << player2.name << "님이 승리!!" << endl;
-                        return; // 3개 동일 숫자일경우 종료
-                    }
-                    else {
-                        cout << "아쉽군요!" << endl;
-                        break;  // 3개 동일숫자 아닐경우 다음 선수로
-                    }
-                }
-                else {
-                    cout << "잘못된 입력입니다. 다시 입력해주세요." << endl;
-                    continue; // 빈값 아닐시 다시반복
                 }
             }
         }
     }
 };
+
 
 int main() {
     srand(static_cast<unsigned int>(time(0))); // 랜덤 시드 초기화
